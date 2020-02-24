d6a1197eebd88adc3f06907ac5b9058e1cd068d8
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:25:33 +0700

    Add file

---
+++ my.js
@@ -0,0 +1,7 @@
+let age=parseInt(prompt('Nhap vao so nguyen de kiem tra'));
+
+if(age<120 && age>0){
+    alert('So da cho DUNG la so tuoi cua mot nguoi');
+} else {
+    alert('So da cho KHONG PHAI la so tuoi cua mot nguoi');
+}
